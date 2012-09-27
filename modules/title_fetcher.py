18c86fb7b0f01b85d297fde4f0f7b57c0318ef7d
Kristian Glass (kg) <none@none>
2012-09-27 16:37:24 +0100

    Fixed up title_fetcher

--- modules/title_fetcher.py
+++ modules/title_fetcher.py
@@ -2,9 +2,10 @@ import util
 from BeautifulSoup import BeautifulSoup as soup
 import requests
 import commands
+import re
 
 @commands.register(re.compile(r'http://[^ $]*'))
-def fetch_title:(callback, m):
+def fetch_title(callback, m):
     url = m.group()
 
     r = requests.get(url)
@@ -15,3 +16,6 @@ def fetch_title:(callback, m):
     results = page.find("title")
     if results is not None:
         return "%s: %s" % (url, util.strip_html(results).decode("utf-8"))
+
+if __name__ == '__main__':
+    print fetch_title(None, re.match(r'http://[^ $]*', "foo http://google.com/ bar"))
