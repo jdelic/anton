ea99ed376bcf5b466992ce48e6a6f87c7b4ce7ee
Kristian Glass <git@doismellburning.co.uk>
2012-10-25 15:40:48 +0100

    Join #laterpay

--- config.py
+++ config.py
@@ -8,6 +8,5 @@ HTTP_LISTEN = "0.0.0.0", int(os.getenv("PORT"))
 BOT_USERNAME = "anton"
 BOT_REALNAME = "anton"
 BOT_NICKNAME = "anton"
-#BOT_CHANNELS = ("#twilightzone", "#laterpay",)
+BOT_CHANNELS = ("#twilightzone", "#laterpay",)
 JENKINS_CHANNEL = os.getenv("JENKINS_CHANNEL")
-BOT_CHANNELS = ("#twilightzone", )
