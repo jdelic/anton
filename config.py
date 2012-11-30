08a456b5ccaf30f426ae7b6e583ceb5fc6016165
Kristian Glass <git@doismellburning.co.uk>
2012-11-30 13:51:48 +0000

    Antonia

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ BACKEND = os.getenv("IRC_SERVER_HOST"), int(os.getenv("IRC_SERVER_PORT"))
 HTTP_ROOT = "/bot"
 HTTP_LISTEN = "0.0.0.0", int(os.getenv("PORT"))
 
-BOT_USERNAME = "anton"
-BOT_REALNAME = "anton"
-BOT_NICKNAME = "anton"
+BOT_USERNAME = "antonia"
+BOT_REALNAME = "antonia"
+BOT_NICKNAME = "antonia"
 BOT_CHANNELS = ("#twilightzone", "#laterpay",)
 JENKINS_CHANNEL = os.getenv("JENKINS_CHANNEL")
 GITHUB_CHANNEL = JENKINS_CHANNEL
