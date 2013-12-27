2c493e273f59a29120a8a125d6a49ad59e53e1c8
Jonas Maurus <jmaurus@laterpay.net>
2013-12-27 15:37:30 +0000

    fix whitespace formatting

--- run.sh
+++ run.sh
@@ -3,28 +3,27 @@
 PATH=/usr/local/bin:/usr/bin:/bin:/usr/games
 PID=$(pgrep -f "python .*holly.py")
 if [ "x$PID" != "x" ]; then
-  echo already running
-  exit 1
+    echo already running
+    exit 1
 fi
 
 if [ "$1" != "run" ]; then
-  SCREENLIST=$(screen -list | grep holly | grep "^\s")
-  if [ "x$SCREENLIST" != "x" ]; then
-    echo screen already running
-    exit 1
-  fi
+    SCREENLIST=$(screen -list | grep holly | grep "^\s")
+    if [ "x$SCREENLIST" != "x" ]; then
+        echo screen already running
+        exit 1
+    fi
 
-  screen -dmS holly $0 run
+    screen -dmS holly $0 run
 else
-  cd /home/holly/holly
-  ulimit -c 999999999
-  . /home/holly/holly_virtualenv/bin/activate
-  while true
-  do
-    python holly.py 2>&1 | tee -a holly.log
-    echo holly crashed, restarting in 5 seconds >> holly.log
-    echo holly crashed, restarting in 5 seconds
-    sleep 5
-  done
+    cd /home/holly/holly
+    ulimit -c 999999999
+    . /home/holly/holly_virtualenv/bin/activate
+    while true; do
+        python holly.py 2>&1 | tee -a holly.log
+        echo holly crashed, restarting in 5 seconds >> holly.log
+        echo holly crashed, restarting in 5 seconds
+        sleep 5
+    done
 fi
 
