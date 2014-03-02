8af3e805497bda80bab6a104f7dd05b07923fc19
Jonas Maurus <jmaurus@laterpay.net>
2014-03-02 16:25:32 +0000

    Let's start by adding documentation, functools.wraps and a bit of support for passing more information to handlers than just the IRC message

--- anton/events.py
+++ anton/events.py
@@ -1,3 +1,12 @@
+# -* coding: utf-8 *-
+"""
+A very simple pubsub event router where anything can register an event "name" (in this module called "type") which it
+wants to listen to by using the `@events.register` decorator and anything else can call all registered listeners by
+calling the `events.fire()` function.
+
+Both `anton.irc_client` and `anton.http` distribute function calls this way when they receive data.
+"""
+
 STOP, CONTINUE = True, False
 
 EVENTS = {}
