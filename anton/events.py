e5be31d668b08a7a61043d90389c5236c5faea3c
Jonas Maurus <jmaurus@laterpay.net>
2015-02-25 00:08:32 +0000

    refactor and add docs to anton.events

--- anton/events.py
+++ anton/events.py
@@ -12,14 +12,24 @@ STOP, CONTINUE = True, False
 EVENTS = {}
 
 
-def register(type):
-    def decorate(fn):
-        EVENTS.setdefault(type, []).append(fn)
-        return fn
+def register(event_type):
+    """
+    Register a handler for events of the type ``event_type``, where ``event_type`` is just a
+    string compared to the ``event_type`` parameter passed to calls to ``events.fire()``.
+    """
+    def decorate(handler):
+        EVENTS.setdefault(event_type, []).append(handler)
+        return handler
 
     return decorate
 
 
-def fire(type, context, obj):
-    for x in EVENTS.get(type, []):
-        x(type, context, obj)
+def fire(event_type, event_context, event_args):
+    """
+    Call all handlers registered using @events.register(event_type) for ``event_type`` in a loop
+    and pass them arbitrary context and event argument information. The content of these
+    parameters differs depending on the module raising the event. See ``anton.commands`` and
+    ``anton.http`` for more information.
+    """
+    for handler in EVENTS.get(event_type, []):
+        handler(event_type, event_context, event_args)
